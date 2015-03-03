a592b939f706fa8b7ddc4427ad4c817b7bff4a6d
rnw99 <ryan.n.wijaya@gmail.com>
2015-03-03 18:05:45 +1100

    Initial Commit

---
+++ week1/day2/RyanApp/RyanApp/ViewControllers/RyanProfileViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RyanProfileViewController.swift
+//  RyanApp
+//
+//  Created by Ryan Wijaya on 1/03/2015.
+//  Copyright (c) 2015 rnwcorp. All rights reserved.
+//
+
+import UIKit
+
+class RyanProfileViewController: UIViewController {
+
+    @IBAction func unwindSegueToRyanProfile(segue: UIStoryboardSegue) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
